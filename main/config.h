535ef3bfdcac0b150a3fb3f1ed6a07bbc2851ffe
cole <atolase120@gmail.com>
2026-09-03 20:55:49 +0100

    added wifi

--- main/config.h
+++ main/config.h
@@ -3,6 +3,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-
+#define WIFI_SSID "momentum"
+#define WIFI_PASS "iamalive"
 
 #endif
--- main/main.c
+++ main/main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
+#include "config.h"
+#include "wifi.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_err.h"
 
+static const char *TAG = "orbit";
 
+esp_err_t start_wifi()
+{
+    esp_err_t err = wifi_init_sta(WIFI_SSID, WIFI_PASS);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Wi-Fi init failed: %s", esp_err_to_name(err));
+        vTaskDelete(NULL);
+        return ESP_OK;
+    }
+    return ESP_OK;
+}
+
+// ******************* main entry ************************
 void app_main(void)
 {
-
+    start_wifi();
 }
